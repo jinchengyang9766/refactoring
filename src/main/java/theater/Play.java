f1df9c379ec138f01f0b154c483c1a9ec8547ea8
dell <@gmai.com>
2025-11-21 15:10:35 -0500

    Task 2.2

--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,25 @@
 package theater;
 
-public class Play {
+/**
+ * Class representing a play.
+ *
+ * @author Your Name
+ */
 
-    public String name;
-    public String type;
+public class Play {
+    private final String name;
+    private final String type;
 
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
     }
 }
